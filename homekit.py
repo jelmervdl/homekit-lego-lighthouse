fa8a91a1c55ff3267f2a75ac14c6790b624f9e09
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2023-12-01 23:35:01 +0000

    Turn motor off if light turns off

--- homekit.py
+++ homekit.py
@@ -38,39 +38,70 @@ class Light(Accessory):
         self.char_brightness = serv_light.configure_char('Brightness', value=100, setter_callback=self._set_brightness)
 
         serv_fan = self.add_preload_service('Fan', chars=['RotationSpeed', 'RotationDirection'])
+        self.char_rotation_on = serv_fan.configure_char('On', setter_callback=self._set_motor_on)
         self.char_rotation_speed = serv_fan.configure_char('RotationSpeed', setter_callback=self._set_rotation_speed)
         self.char_rotation_direction = serv_fan.configure_char('RotationDirection', setter_callback=self._set_rotation_direction)
     
     def _set_on(self, on:bool):
         if on:
             self.light.on()
+            if self._speed > 0:
+                self.char_rotation_on.value = True
+            self._update_motor()
         else:
             self.light.off()
+            self.char_rotation_on.value = False
+            self.motor.stop()
 
     def _set_brightness(self, brightness:int):
         self.light.brightness(brightness)
 
+    def _set_motor_on(self, on:bool):
+        if on:
+            self._update_motor()
+        else:
+            self.motor.stop()
+
     def _set_rotation_speed(self, speed:float):
         self._speed = speed
-        self.motor.start(self._direction * self._speed)
+        self._update_motor()
 
     def _set_rotation_direction(self, direction):
         self._direction = 1 if direction == 1 else -1
+        self._update_motor()
+
+    def _update_motor(self):
         self.motor.start(self._direction * self._speed)
 
     def stop(self):
         self.light.off()
         self.motor.stop()
 
-# Start the accessory on port 51826
-driver = AccessoryDriver(port=51826)
+def main():
+    # Start the accessory on port 51826
+    driver = AccessoryDriver(port=51826)
 
-# Change `get_accessory` to `get_bridge` if you want to run a Bridge.
-driver.add_accessory(accessory=Light(driver, 'Lighthouse'))
+    # Change `get_accessory` to `get_bridge` if you want to run a Bridge.
+    driver.add_accessory(accessory=Light(driver, 'Lighthouse'))
 
-# We want SIGTERM (terminate) to be handled by the driver itself,
-# so that it can gracefully stop the accessory, server and advertising.
-signal.signal(signal.SIGTERM, driver.signal_handler)
+    # We want SIGTERM (terminate) to be handled by the driver itself,
+    # so that it can gracefully stop the accessory, server and advertising.
+    signal.signal(signal.SIGTERM, driver.signal_handler)
 
-# Start it!
-driver.start()
+    # Start it!
+    driver.start()
+
+
+retries = 5
+
+while True:
+    try:
+        main()
+    except (OSError, buildhat.exc.BuildHATError):
+        if retries > 0:
+            retries -= 1
+            time.sleep(10)
+        else:
+            raise
+
+
